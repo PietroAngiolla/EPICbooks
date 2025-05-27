15bc3e8e722d638acb3fab8963c88bbd9d72c82f
PietroAngiolla <pietroangiolla@gmail.com>
2025-05-27 18:34:11 +0200

    Books loaded

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
-const STRIVESCHOOL_BOOKS_API="https://www.google.com/url?q=https://striveschool-api.herokuapp.com/books&sa=D&source=editors&ust=1748344806302786&usg=AOvVaw0KUA59-pu8BAk7kERN8rAl"
+const STRIVESCHOOL_BOOKS_API = "https://striveschool-api.herokuapp.com/books"
 
+async function getAllBooks() {
+    try {
+        const result = await fetch(STRIVESCHOOL_BOOKS_API)
+        const json = await result.json()
+        console.log(json)
+        const bookList = document.querySelector(".row")
+        json.forEach(element => {
+            const cardHTML =`
+            <div class="card col-xs-6 col-md-3 col-lg-2" style="width: 18rem;">
+                <img src= ${element.img} class="card-img-top" alt = "title: ${element.title}">
+                <div class="card-body">
+                    <h5 class="card-title">Title: ${element.title}</h5>
+                    <p class="card-text">Genre: ${element.category}</p>
+                    <p class="card-text">Price: ${element.price}</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>`
+            bookList.innerHTML+=cardHTML
+            
+        });
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+getAllBooks()
